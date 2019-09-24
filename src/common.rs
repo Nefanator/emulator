7ecf0cfb74bec14afafa17f07b527bc6ab53e2bc
Nathan Franklin <Nathan.Franklin@outlook.com>
2019-09-24 15:40:06 +0100

    Added unit test to common

--- src/common.rs
+++ src/common.rs
@@ -12,7 +12,23 @@ pub struct Pin<F> {
     falling_edge_events: Cell<Vec<F>>
 }
 
-impl<F> Pin<F> {
+impl<F> Pin<F>
+where F: FnMut()
+{
+    fn new() -> Self {
+        let providing_power = false;
+        let links_providing_power = Cell::default();
+        let raising_edge_events = Cell::default();
+        let falling_edge_events = Cell::default();
+
+        Pin {
+            providing_power,
+            links_providing_power,
+            raising_edge_events,
+            falling_edge_events
+        }
+    }
+
     fn set(&mut self, provide_power: bool) {
         if provide_power != self.providing_power {
             self.providing_power = provide_power;
@@ -25,11 +41,31 @@ impl<F> Pin<F> {
         unimplemented!()
     }
 
-    fn on_raising_edge(&mut self, f: F) {
+    fn on_raising_edge(&mut self, f: F)
+    {
         self.raising_edge_events.get_mut().push(f);
     }
 
     fn on_falling_edge(&mut self, f: F) {
         self.falling_edge_events.get_mut().push(f);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut pin1 = Pin::new();
+        let mut pin2 = Pin::new();
+        let mut value = false;
+
+        pin1.link(pin2);
+
+        pin1.on_raising_edge(move || value = true);
+        pin2.set(true);
+
+        assert!(value);
     }
 }
